3bcb4736edea4cd8986a213d8d4f5553d45e19dd
diogoizele <diogogabrielizele@gmail.com>
2023-02-23 13:01:30 -0300

    adiciona primeiro exemplo em c++ de algoritmos 1

---
+++ 2023-01/algoritmos-1/aula-2/exemplo-1.cpp
@@ -0,0 +1,22 @@
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  int v1, v2, soma;
+
+  cout << "Digite o primeiro valor: ";
+  cin >> v1;
+
+  cout << "Digite o segundo valor: ";
+  cin >> v2;
+
+  soma = v1 + v2;
+
+  cout << "A soma dos valores é: " << soma << endl;
+
+  return 0;
+}
