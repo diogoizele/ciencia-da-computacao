eee0d7873cd6439563ecb5e3f1e47467ca3f8bec
diogoizele <diogogabrielizele@gmail.com>
2023-03-02 22:15:39 -0300

    adiciona atividade 1 e 2 de circuitos digitais

---
+++ 2023-01/algoritmos-1/aula-3/exercicios/lista-2-exercicio-1.cpp
@@ -0,0 +1,16 @@
+/**
+ * Faça um programa que receba o preço de um produto, 
+ * calcule e mostre o novo preço, sabendo-se que este
+ * sofreu um desconto de 10 %.
+*/
+
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+
+	return 0;
+}
